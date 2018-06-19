d36458b9890a6372cd9bad2e4593db0762b6b770
maodq <noiary@126.com>
2018-06-20 00:03:09 +0800

    add page: Animation demo

--- lib/demo/app.dart
+++ lib/demo/app.dart
@@ -1,37 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterfemo/demo/Demo.dart';
+import 'package:flutterfemo/demo/demo.dart';
 import 'package:flutterfemo/demo/Listdemo.dart';
 import 'package:flutterfemo/demo/home.dart';
 import 'package:flutterfemo/page/hello_world.dart';
 
-class MyApp extends StatelessWidget {
-  static final String HELLO_WORLD = "/demo/hello";
-  static final String LIST_VIEW = "/demo/list";
-  static final String CONTAINER = "/demo/container";
-  static final String IMAGE = "/demo/image";
+final String HELLO_WORLD = "/demo/hello";
+final String LIST_VIEW = "/demo/list";
+final String CONTAINER = "/demo/container";
+final String IMAGE = "/demo/image";
+final String ANIMATION = "/demo/animation";
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Flutter Demo",
-
       theme: new ThemeData(
-        primaryColor: Colors.white,
-        platform: TargetPlatform.iOS
-      ),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("App Bar"),
-        ),
-        body: createBody(),
-      ),
+          primaryColor: Colors.white, platform: TargetPlatform.iOS),
+      home: new MyHome(),
       routes: <String, WidgetBuilder>{
         HELLO_WORLD: (_) => new HelloWorld(),
         LIST_VIEW: (_) => new ListDemo(),
         CONTAINER: (_) => new ContainerDemo(),
         IMAGE: (_) => new ImageDemo(),
+        ANIMATION: (_) => new AnimationDemo(),
       },
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyState();
+  }
+}
+
+class MyState extends State<MyHome> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("App Bar"),
+      ),
+      body: createBody(),
     );
   }
 
@@ -43,6 +56,7 @@ class MyApp extends StatelessWidget {
         new HomeItem("ListView", LIST_VIEW),
         new HomeItem("Container", CONTAINER),
         new HomeItem("Image", IMAGE),
+        new HomeItem("animation", ANIMATION),
       ].map((Widget homeItem) {
         return new Container(
           padding: const EdgeInsets.symmetric(vertical: 12.0),
